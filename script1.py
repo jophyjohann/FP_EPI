26d1e83b4393186c58e81f05d95180b635b4d853
jophyjohann <94175623+jophyjohann@users.noreply.github.com>
2021-12-11 19:13:38 +0000

    x

--- script1.py
+++ script1.py
@@ -7,16 +7,21 @@ class run:
 		self.dat = DataSet_Operations()
 		self.dat.import_dataset_measurements()
 
+		self.export_folder = "export_script1/"
+		self.export_extension = ".pdf"
+
 
 	def main(self):
 		dataSet = self.dat.dataSet
 
+		### Plot all measured datasets (11) and export them for overwiev ###
 
 		for data in dataSet:
 			print(80*"_"+"\n\nPlotting: ",data['name'])
 		
 			fig = plt.figure(figsize=(8, 4), dpi=120).add_subplot(1, 1, 1)
-			plt.plot(data['x'], data['y'], '.', label='Untergrund Messung')
+			plt.plot(data['x'], data['y'], '.')
 			plt.title(label=data['name'][24:-20])
+			plt.savefig(self.export_folder+data['name'][24:-20]+self.export_extension, bbox_inches='tight')
 			plt.show()
 
--- script2.py
+++ script2.py
@@ -7,8 +7,22 @@ class run:
 		self.dat = DataSet_Operations()
 		self.dat.import_dataset_measurements()
 
+		self.export_folder = "export_script1/"
+		self.export_extension = ".pdf"
+
 
 	def main(self):
 		dataSet = self.dat.dataSet
 
-		print("script2.py is running!")
+
+		### Plot all measured datasets (11) and export them for overwiev ###
+		
+		data = dataSet[0]
+		print(80*"_"+"\n\nPlotting: ",data['name'])
+		
+		fig = plt.figure(figsize=(8, 4), dpi=120).add_subplot(1, 1, 1)
+		plt.plot(data['x'], data['y'], '.')
+		plt.title(label=data['name'][24:-20])
+		plt.savefig(self.export_folder+data['name'][24:-20]+self.export_extension, bbox_inches='tight')
+		plt.show()
+
