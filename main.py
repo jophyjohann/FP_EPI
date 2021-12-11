becffa7cd2a6fef4999268d4fb063639cdef0efa
jophyjohann <94175623+jophyjohann@users.noreply.github.com>
2021-12-11 19:03:48 +0000

    x

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from dataset_operations import DataSet_Operations
+import importlib
 
+'''
+for i in range(1,3):
+			exec("import script"+str(i))
+			#exec("self.s"+str(i)+"=script"+str(i)+".run()")
+'''
 
 class run:
 
@@ -7,7 +13,7 @@ class run:
 		self.dat = DataSet_Operations()
 
 		self.n_scripts = 2
-
+		
 		for i in range(1,self.n_scripts+1):
 			exec("import script"+str(i))
 			exec("self.s"+str(i)+"=script"+str(i)+".run()")
@@ -20,14 +26,23 @@ class run:
 		x = input()
 		while(True):
 			if x.isdigit() and int(x) <= run.n_scripts and int(x) > 0:
-				exec("run.s"+x+".main()")
+				exec("import script"+x)
+				exec("importlib.reload(script"+x+")")
+				exec("self.s"+x+"=script"+x+".run()")
+				exec("self.s"+x+".main()")
 			elif x[:-1].isdigit() and x[-1:]=="d" and int(x[:-1]) <= run.n_scripts and int(x[:-1]) > 0:
 				while(True):
-					exec("run.s"+x[:-1]+".main()")
+					exec("import script"+x[:-1])
+					exec("importlib.reload(script"+x[:-1]+")")
+					exec("self.s"+x[:-1]+"=script"+x[:-1]+".run()")
+					exec("self.s"+x[:-1]+".main()")
 					print(80*"_"+"\nStarting script again..")
 			else:
 				print('Loading all..\n')
 				for i in range(run.n_scripts):
+					exec("import script"+str(i+1))
+					exec("importlib.reload(script"+str(i+1)+")")
+					exec("self.s"+str(i+1)+"=script"+str(i+1)+".run()")
 					exec("run.s"+str(i+1)+".main()")
 			print('\nExecuted sucessfully...')
 			y = input()
--- script1.py
+++ script1.py
@@ -11,30 +11,12 @@ class run:
 	def main(self):
 		dataSet = self.dat.dataSet
 
-		print(80*"_"+"\n\nPlotting: ",dataSet[0]['name'])
 
-		fig = plt.figure(figsize=(8, 4), dpi=120).add_subplot(1, 1, 1)
-		plt.plot(dataSet[0]['x'], dataSet[0]['y'], '.', label='Untergrund Messung')
-		plt.title()
-		plt.show()
+		for data in dataSet:
+			print(80*"_"+"\n\nPlotting: ",data['name'])
+		
+			fig = plt.figure(figsize=(8, 4), dpi=120).add_subplot(1, 1, 1)
+			plt.plot(data['x'], data['y'], '.', label='Untergrund Messung')
+			plt.title(label=data['name'][24:-20])
+			plt.show()
 
-
-		print(80*"_"+"\n\nPlotting: ",dataSet[1]['name'])
-
-		fig = plt.figure(figsize=(8, 4), dpi=120).add_subplot(1, 1, 1)
-		plt.plot(dataSet[1]['x'], dataSet[1]['y'], '.', label='Doppelspalt')
-		plt.show()
-
-
-		print(80*"_"+"\n\nPlotting: ",dataSet[2]['name'])
-
-		fig = plt.figure(figsize=(8, 4), dpi=120).add_subplot(1, 1, 1)
-		plt.plot(dataSet[2]['x'], dataSet[2]['y'], '.', label='Linker Spalt')
-		plt.show()
-
-
-		print(80*"_"+"\n\nPlotting: ",dataSet[3]['name'])
-
-		fig = plt.figure(figsize=(8, 4), dpi=120).add_subplot(1, 1, 1)
-		plt.plot(dataSet[3]['x'], dataSet[3]['y'], '.', label='Rechter Spalt')
-		plt.show()
