1b99945b133fb275afae0bec36eda0b806d9b634
jophyjohann <94175623+jophyjohann@users.noreply.github.com>
2021-12-22 19:38:54 +0000

    x

--- script9.py
+++ script9.py
@@ -22,22 +22,15 @@ class run:
 			mng.resize(*mng.window.maxsize())
     
 		dataSet_No = 4  # Dunkelmessung PM
-		data1 = dataSet[dataSet_No]
-		name1 = data1['name'][24:-20]
+		data = dataSet[dataSet_No]
+		name = data['name'][24:-20]
 		
-		print(50*"_"+"\n\nPlotting: ", name1.replace("_"," "))
+		print(50*"_"+"\n\nPlotting: ", name.replace("_"," "))
 
-		dataSet_No = 5 # Hellmessung  PM
-		data2 = dataSet[dataSet_No]
-		name2 = data2['name'][24:-20]
-		
-		print(50*"_"+"\n\nPlotting: ", name2.replace("_"," "))
-
-		plt.plot(data1['x'], data1['y'], '-', label='name1')
-		plt.plot(data2['x'], data2['y'], '-', label='name2')
+		plt.plot(data['x'], data['y'], '-', label='name1')
 		plt.plot([0.55, 0.55], [0, 1500], 'r--')
-		plt.title(label=name1.replace("_"," "))
+		plt.title(label=name.replace("_"," "))
 		plt.ylim(0, 1400)
-		plt.savefig(self.export_folder + 'Arbeitsspannung' + self.export_extension, bbox_inches='tight')
+		plt.savefig(self.export_folder + 'name123' + self.export_extension, bbox_inches='tight')
 		maximize()
 		plt.show()
